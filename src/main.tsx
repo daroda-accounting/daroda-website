49840654d5e548942ddda0d67209b7a7a7d64731
ArchILLtect <nickhansonsr@gmail.com>
2026-03-30 10:12:14 -0500

    feat: refactor main entry point to use AppRouter with ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { AppRouter } from "./routes/AppRouter";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider value={(defaultSystem)}>
+      <AppRouter />
+    </ChakraProvider>
+  </React.StrictMode>
+);
